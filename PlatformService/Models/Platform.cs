08e227be33b1f2cfae35871fda2d78a7f3bf73fb
Omar <saucyfunnel@gmail.com>
2023-08-28 21:17:37 -0500

    conf

--- PlatformService/Models/Platform.cs
+++ PlatformService/Models/Platform.cs
@@ -2,5 +2,9 @@
 {
     public class Platform
     {
+        public int MyProperty { get; set; }
+        public string MyProperty1 { get; set; }
+        public string MyProperty2 { get; set; }
+        public string Cost { get; set; }
     }
 }
