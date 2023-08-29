2e9855034995ebf68ad76bedc9fad5dcd5b92665
Omar <saucyfunnel@gmail.com>
2023-08-29 07:59:56 -0500

    2do dia

--- PlatformService/Models/Platform.cs
+++ PlatformService/Models/Platform.cs
@@ -3,8 +3,8 @@
     public class Platform
     {
         public int MyProperty { get; set; }
-        public string MyProperty1 { get; set; }
-        public string MyProperty2 { get; set; }
+        public string Name { get; set; }
+        public string Publisher { get; set; }
         public string Cost { get; set; }
     }
 }
