5c7044a74b2272549b4d0dda1b168f61b66a17b1
Omar <saucyfunnel@gmail.com>
2023-12-06 16:22:51 -0500

    actualizado

---
+++ PlatformService/Controllers/PlatformsController.cs
@@ -0,0 +1,14 @@
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PlatformService.Data;
+
+namespace PlatformService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+        private readonly IPlatformRepo _repository;
+        private readonly IMapper _mapper;
+    }
+}
